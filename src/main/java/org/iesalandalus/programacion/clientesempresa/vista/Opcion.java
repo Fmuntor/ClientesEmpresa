438f699dea57f7e5833ff8d0ad27687158638329
Fmuntor <dam209f@gmail.com>
2023-01-14 17:27:34 +0100

    Clase Opcion

---
+++ src/main/java/org/iesalandalus/programacion/clientesempresa/vista/Opcion.java
@@ -0,0 +1,11 @@
+package org.iesalandalus.programacion.clientesempresa.vista;
+
+public enum Opcion {
+    
+    BORRAR_CLIENTE,
+    BUSCAR_CLIENTE,
+    INSERTAR_CLIENTE,
+    MOSTRAR_CLIENTE,
+    MOSTRAR_CLIENTES_FECHA,
+    SALIR
+}
